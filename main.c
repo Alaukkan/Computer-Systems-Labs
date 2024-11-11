d53936a7b1d54c803a89769fa3255709598a9eb3
alexlaukkanen <alexlaukkanen@gmail.com>
2024-11-11 16:11:07 +0200

    continued developement
    Simple movement detection for dot and space and translation done, some
    sounds incorporated. TODO: add dash, improve movement detection
    algorithm to use the buffer sensor_buffer instead of single values.

--- main.c
+++ main.c
@@ -5,9 +5,11 @@
  *      Author: alexl
  */
 
+#include <library.h>
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
 
 /* XDCtools files */
 #include <xdc/std.h>
@@ -31,22 +33,18 @@
 #include "buzzer.h"
 
 /* Task */
-#define STACKSIZE 5096
+#define STACKSIZE 2048
 Char sensorTaskStack[STACKSIZE];
-Char uartTaskStack[STACKSIZE];
-Char buzzerTaskStack[1024];
+Char uartTaskStack[STACKSIZE * 2];
+Char buzzerTaskStack[STACKSIZE];
 
-
-enum state { WAITING=1, READY };
+enum state { WAITING=0, READY };
 enum state programState = WAITING;
 enum state dataState = WAITING;
 
-float timestamp, ax, ay, az, gx, gy, gz;
+struct mpu_sample_t sensor_buffer[3]; // buffer saves last 3 recorded data samples
 
-Char message = ' ';
-
-enum melody {OFF=1, MELODY_START_UP, MELODY_SHUT_DOWN};
-enum melody playing_melody = OFF;
+struct melody_t * playing_melody;
 
 static PIN_Handle buttonHandle;
 static PIN_State buttonState;
@@ -83,19 +81,41 @@ static const I2CCC26XX_I2CPinCfg i2cMPUCfg = {
     .pinSCL = Board_I2C0_SCL1
 };
 
-Void buzzer(int m);
+bool checkTilt(int * tilt, struct mpu_sample_t samples[]);
+bool checkJolt(int * jolt, struct mpu_sample_t samples[]);
+char checkMovement(int * movement, struct mpu_sample_t samples[]);
+Void sensorInit(I2C_Handle *i2c, I2C_Params *i2cParams);
+
 
 void buttonFxn(PIN_Handle handle, PIN_Id pinId) {
     uint_t pinValue = PIN_getOutputValue(Board_LED0);
     pinValue = !pinValue;
-    programState = !programState;
     PIN_setOutputValue(ledHandle, Board_LED0, pinValue);
     if (programState == WAITING) {
-        //playing_melody = MELODY_SHUT_DOWN;
-        buzzer(MELODY_SHUT_DOWN);
+        playing_melody = startup;
+        programState = READY;
     } else {
-        //playing_melody = MELODY_START_UP;
-        buzzer(MELODY_START_UP);
+        playing_melody = shutdown;
+        programState = WAITING;
+    }
+}
+
+Void buzzerTaskFxn(UArg arg0, UArg arg1) {
+    while (1) {
+        if (playing_melody != NULL) {
+            buzzerOpen(hBuzzer);
+            int i = 0;
+
+            while (playing_melody[i].duration > 0) {
+                buzzerSetFrequency(playing_melody[i].frequency);
+                Task_sleep(playing_melody[i].duration * 1000 / Clock_tickPeriod);
+                i++;
+            }
+
+            buzzerClose();
+            playing_melody = NULL;
+        }
+        Task_sleep(50000 / Clock_tickPeriod);
     }
 }
 
@@ -118,49 +138,169 @@ Void uartTaskFxn(UArg arg0, UArg arg1) {
         System_abort("Error opening the UART");
     }
 
+    int movement[2] = {-1, 0};
+    char curr_message[6];
+    int message_len = 0;
+
     while (1) {
         if (programState == WAITING) {
             Task_sleep(50000/ Clock_tickPeriod);
             continue;
         }
 
-        /*char str[6];
-        if (programState == DATA_READY) {
-            sprintf(str, "%c\n\r", message);
-            System_printf(str);
-            System_flush();
-            UART_write(uart, str, strlen(str));
-            programState = WAITING;
-        }
-
-        System_printf("uartTask\n");
-        System_flush();*/
-
-        char str2[64];
+        /* FOR DETECTING MOVEMENT */
         if (dataState == READY) {
-            sprintf(str2, "%f,%3.2f,%3.2f,%3.2f,%3.2f,%3.2f,%3.2f\n\r", timestamp, ax, ay, az, gx, gy, gz);
-            UART_write(uart, str2, strlen(str2));
+
+            /*Sanity check
+            char str1[32];
+            if (tilt) {
+                sprintf(str1, "Tilt detected %d\n\r", tilt);
+            } else {
+                sprintf(str1, "Device is idle\n\r");
+            }
+            UART_write(uart, str1, strlen(str1) + 1);
+            */
+
+            char morse = checkMovement(movement, sensor_buffer);
+
+            if (morse != '\0') {
+                char str[4] = {morse, '\n', '\r', '\0'};
+                UART_write(uart, str, 4);
+                switch(morse){
+                case('.'):
+                    playing_melody = dot;
+                    break;
+                case('-'):
+                    playing_melody = dash;
+                    break;
+                }
+
+                if (morse == ' ') { // translating morse to latin
+                    int i;
+                    if (strlen(curr_message) == 5) {
+                        for (i = 0; i < 10; i++) {
+                            if (strcmp(num_translate[i].morse, curr_message) == 0) {
+                                char str[4] = {num_translate[i].number, '\n', '\r', '\0'};
+                                UART_write(uart, str, 4);
+                                break;
+                            }
+                        }
+                    } else {
+                        for (i = 0; i < 26; i++) {
+                            if (strcmp(translate[i].morse, curr_message) == 0) {
+                                char str[4] = {translate[i].latin, '\n', '\r', '\0'};
+                                UART_write(uart, str, 4);
+                                break;
+                            }
+                        }
+                    }
+                    curr_message[0] = '\0';
+                    message_len = 0;
+                } else if (message_len < 5) {
+                    curr_message[message_len + 1] = curr_message[message_len];
+                    curr_message[message_len] = morse;
+                    message_len++;
+                } else {
+                    curr_message[0] = '\0';
+                    message_len = 0;
+                }
+            }
+
+
             dataState = WAITING;
         }
-
-        //System_printf("uartTask\n");
-        //System_flush();
+        /* FOR COLLECTING DATA
+        char str1[64];
+        if (dataState == READY) {
+            sprintf(str1, "%f,%3.2f,%3.2f,%3.2f,%3.2f,%3.2f,%3.2f\n\r", sensor_buffer[2].timestamp, sensor_buffer[2].accel.ax, sensor_buffer[2].accel.ay, sensor_buffer[2].accel.az, sensor_buffer[2].gyro.gx, sensor_buffer[2].gyro.gy, sensor_buffer[2].gyro.gz);
+            UART_write(uart, str1, strlen(str1) + 1);
+            dataState = WAITING;
+        }*/
 
         // Once per 50ms
         Task_sleep(50000/ Clock_tickPeriod);
     }
 }
 
+bool checkTilt(int * tilt, struct mpu_sample_t samples[]) {
+    float diff_gy = tilt_data[0].gyro.gy - samples[2].gyro.gy;
+    int threshold = 170;
+    if (*tilt == 0) {
+        if (diff_gy > threshold + 50) {
+            (*tilt)++;
+        } else {
+            *tilt = 0;
+        }
+    } else if (*tilt < 4) {
+        if (diff_gy < -threshold) {
+            return true;
+        } else {
+            (*tilt)++;
+        }
+    }
+    return false;
+}
 
+bool checkJolt(int * jolt, struct mpu_sample_t samples[]) {
+    float diff_ay = tilt_data[0].accel.ay - samples[2].accel.ay;
+    float threshold = 0.6;
+    if (*jolt == 0) {
+        if (diff_ay > threshold + 0.2) {
+            (*jolt)++;
+        } else {
+            *jolt = 0;
+        }
+    } else if (*jolt < 4) {
+        if (diff_ay < -threshold) {
+            return true;
+        } else {
+            (*jolt)++;
+        }
+    }
+    return false;
+}
 
-Void sensorTaskFxn(UArg arg0, UArg arg1) {
-    I2C_Handle i2cMPU; // Own i2c-interface for MPU9250 sensor
-    I2C_Params i2cMPUParams;
+char checkMovement(int * movement, struct mpu_sample_t samples[]) {
+    char result = '\0';
+    if (movement[0] == -1 || movement[0] == 0) {
+        // dot
+        if (checkTilt(&movement[1], samples)) {
+            result = '.';
+            movement[0] = -1;
+            movement[1] = 0;
+        } else if (movement[1] > 0 && movement[1] < 4) {
+            movement[0] = 0;
+        } else {
+            movement[0] = -1;
+            movement[1] = 0;
+        }
+    }
+    if (movement[0] == -1 || movement[0] == 1) {
+        // dash
+        if (checkJolt(&movement[1], samples)) {
+            result = ' ';
+            movement[0] = -1;
+            movement[1] = 0;
+        } else if (movement[1] > 0 && movement[1] < 4) {
+            movement[0] = 1;
+        } else {
+            movement[0] = -1;
+            movement[1] = 0;
+        }
+    }
+    if (movement[0] == -1 || movement[0] == 2) {
+        // space
 
-    I2C_Params_init(&i2cMPUParams);
-    i2cMPUParams.bitRate = I2C_400kHz;
+    }
+    return result;
+}
+
+Void sensorInit(I2C_Handle *i2c, I2C_Params *i2cParams) {
+
+    I2C_Params_init(i2cParams);
+    i2cParams->bitRate = I2C_400kHz;
     // Note the different configuration below
-    i2cMPUParams.custom = (uintptr_t)&i2cMPUCfg;
+    i2cParams->custom = (uintptr_t)&i2cMPUCfg;
 
     // MPU power on
     PIN_setOutputValue(hMpuPin,Board_MPU_POWER, Board_MPU_POWER_ON);
@@ -171,8 +311,8 @@ Void sensorTaskFxn(UArg arg0, UArg arg1) {
     System_flush();
 
     // MPU open i2c
-    i2cMPU = I2C_open(Board_I2C, &i2cMPUParams);
-    if (i2cMPU == NULL) {
+    *i2c = I2C_open(Board_I2C, i2cParams);
+    if (*i2c == NULL) {
         System_abort("Error Initializing I2CMPU\n");
     }
 
@@ -180,63 +320,34 @@ Void sensorTaskFxn(UArg arg0, UArg arg1) {
     System_printf("MPU9250: Setup and calibration...\n");
     System_flush();
 
-    mpu9250_setup(&i2cMPU);
+    mpu9250_setup(i2c);
 
     System_printf("MPU9250: Setup and calibration OK\n");
     System_flush();
+}
 
+Void sensorTaskFxn(UArg arg0, UArg arg1) {
+    I2C_Handle i2cMPU; // Own i2c-interface for MPU9250 sensor
+    I2C_Params i2cMPUParams;
+
+    sensorInit(&i2cMPU, &i2cMPUParams);
     while (1) {
         if (programState == WAITING) {
             Task_sleep(50000/ Clock_tickPeriod);
             continue;
         }
-        // MPU ask data
-        mpu9250_get_data(&i2cMPU, &ax, &ay, &az, &gx, &gy, &gz);
-        uint32_t ticks = Clock_getTicks();;
-        timestamp = (float) ticks * (Clock_tickPeriod / 1000000.0);
-        dataState = READY;
+        if (dataState == WAITING) {
+            // update buffer
+            sensor_buffer[0] = sensor_buffer[1];
+            sensor_buffer[1] = sensor_buffer[2];
+            // MPU ask data
+            mpu9250_get_data(&i2cMPU, &(sensor_buffer[2].accel.ax), &(sensor_buffer[2].accel.ay), &(sensor_buffer[2].accel.az), &(sensor_buffer[2].gyro.gx), &(sensor_buffer[2].gyro.gy), &(sensor_buffer[2].gyro.gz));
+            uint32_t ticks = Clock_getTicks();
+            sensor_buffer[2].timestamp = (float) ticks * (Clock_tickPeriod / 1000000.0);
+            dataState = READY;
+        }
 
         Task_sleep(50000 / Clock_tickPeriod);
-    }
-}
-
-Void buzzerTaskFxn(UArg arg0, UArg arg1) {
-    while (1) {
-        switch(playing_melody) {
-            case(OFF):
-                break;
-            case(MELODY_START_UP):
-                buzzerOpen(hBuzzer);
-                buzzerSetFrequency(2000);
-                Task_sleep(100000 / Clock_tickPeriod);
-                buzzerClose();
-                playing_melody = OFF;
-                break;
-            case(MELODY_SHUT_DOWN):
-
-                break;
-            default:
-                break;
-        }
-        Task_sleep(100000 / Clock_tickPeriod);
-    }
-}
-
-Void buzzer(int m) {
-    switch(m) {
-        case(OFF):
-            break;
-        case(2):
-            buzzerOpen(hBuzzer);
-            buzzerSetFrequency(2000);
-            Task_sleep(500000 / Clock_tickPeriod);
-            buzzerClose();
-            break;
-        case(MELODY_SHUT_DOWN):
-
-            break;
-        default:
-            break;
     }
 }
 
@@ -247,8 +358,8 @@ Int main(void) {
     Task_Params sensorTaskParams;
     Task_Handle uartTaskHandle;
     Task_Params uartTaskParams;
-    //Task_Handle buzzerTaskHandle;
-    //Task_Params buzzerTaskParams;
+    Task_Handle buzzerTaskHandle;
+    Task_Params buzzerTaskParams;
 
     // Initialize board
     Board_initGeneral();
@@ -257,7 +368,6 @@ Int main(void) {
 
     Board_initI2C();
 
-    // Open MPU power pin
     hMpuPin = PIN_open(&MpuPinState, MpuPinConfig);
     if (hMpuPin == NULL) {
         System_abort("Pin open failed!");
@@ -267,6 +377,7 @@ Int main(void) {
     if(!buttonHandle) {
        System_abort("Error initializing button pins\n");
     }
+
     ledHandle = PIN_open(&ledState, ledConfig);
     if(!ledHandle) {
         System_abort("Error initializing LED pins\n");
@@ -281,7 +392,6 @@ Int main(void) {
         System_abort("Pin open failed!");
     }
 
-
     /* Tasks */
     Task_Params_init(&sensorTaskParams);
     sensorTaskParams.stackSize = STACKSIZE;
@@ -295,20 +405,20 @@ Int main(void) {
     Task_Params_init(&uartTaskParams);
     uartTaskParams.stackSize = STACKSIZE;
     uartTaskParams.stack = &uartTaskStack;
-    uartTaskParams.priority = 2;
+    uartTaskParams.priority = 1;
     uartTaskHandle = Task_create(uartTaskFxn, &uartTaskParams, NULL);
     if (uartTaskHandle == NULL) {
         System_abort("Task create failed!");
     }
-    /*
+
     Task_Params_init(&buzzerTaskParams);
     buzzerTaskParams.stackSize = STACKSIZE;
     buzzerTaskParams.stack = &buzzerTaskStack;
-    buzzerTaskParams.priority = 2;
-    buzzerTaskHandle = Task_create(buzzerTaskFxn, &buzzerTaskParams, NULL);
+    buzzerTaskParams.priority = 3;
+    buzzerTaskHandle = Task_create((Task_FuncPtr)buzzerTaskFxn, &buzzerTaskParams, NULL);
     if (buzzerTaskHandle == NULL) {
         System_abort("Task create failed!");
-    }*/
+    }
 
     /* Sanity check */
     System_printf("Hello world!\n");
@@ -319,14 +429,3 @@ Int main(void) {
 
     return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
